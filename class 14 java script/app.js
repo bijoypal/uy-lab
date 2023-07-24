25bc82c94eddd1a1a41b8551440aff28ca66087f
bijoypal <bijoyfpi74@gmail.com>
2023-07-24 21:20:48 +0600

    class 14 + 05 psd start

---
+++ class 14 java script/app.js
@@ -0,0 +1,27 @@
+document.write(Date());
+// window.alert("Accept");
+// alert("Warning");
+
+console.log(18+12);
+
+document.querySelector('h1').innerHTML="i am from js ";
+
+var hello = "<h1> Hello bijoy</h1>";
+// var, const,let
+
+document.write(hello);
+
+var friend = {
+    fname: 'habib',
+    age: 27,
+    habbies: 'watching movie',
+    occupation: 'student'
+}
+
+console.log(friend.habbies);
+
+if ('usser' == 'user'){
+    document.write('welcome admin')
+} else{
+    document.write('sorry, not allowed')
+}
