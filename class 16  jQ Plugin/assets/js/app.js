17fcb549f4b1631eacdd600349cfdbbb2ab0f787
bijoypal <bijoyfpi74@gmail.com>
2023-08-03 08:49:56 +0600

    c 16 jQ plugin complete

--- class 16  jQ Plugin/assets/js/app.js
+++ class 16  jQ Plugin/assets/js/app.js
@@ -41,5 +41,78 @@ $(function(){
     });
     // venobox end
     
+
+    //COunter UP start
+    $('.counter').counterUp({
+        delay: 10,
+        time: 100000
+    });
+    //COunter UP end
+
+    // countdown start 
+    (() => {
+        // Specify the deadline date
+        const deadlineDate = new Date('August 10, 2023 23:59:59').getTime();
     
+        // Cache all countdown boxes into consts
+        const countdownDays = document.querySelector('.countdown__days .number');
+        const countdownHours = document.querySelector('.countdown__hours .number');
+        const countdownMinutes = document.querySelector('.countdown__minutes .number');
+        const countdownSeconds = document.querySelector('.countdown__seconds .number');
+    
+        // Update the count down every 1 second (1000 milliseconds)
+        setInterval(() => {
+            // Get current date and time
+            const currentDate = new Date().getTime();
+    
+            // Calculate the distance between current date and time and the deadline date and time
+            const distance = deadlineDate - currentDate;
+    
+            // Calculations the data for remaining days, hours, minutes and seconds
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+            const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+            const seconds = Math.floor(distance % (1000 * 60) / 1000);
+    
+            // Insert the result data into individual countdown boxes
+            countdownDays.innerHTML = days;
+            countdownHours.innerHTML = hours;
+            countdownMinutes.innerHTML = minutes;
+            countdownSeconds.innerHTML = seconds;
+        }, 1000);
+    })();
+    // countdown end
+
+    // slider-> slick Slider, Owl Carousel, Swiper JS,
+    // swiper slider start
+        // default:
+        // var swiper = new Swiper(".mySwiper", {});
+        // navigation:
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            loop: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+            
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                // type: "progressbar",
+              },
+          });
+       
+
+    // swefer slider end
+
+    
+
 });
+
+
