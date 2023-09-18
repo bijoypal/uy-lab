a7805875eecaf1fb4a0e63d46b0d80547ca5ab84
bijoypal <bijoyfpi74@gmail.com>
2023-09-18 20:01:42 +0600

    all psd sites resposibe & slide complete

---
+++ PSD to HTML works/05-main/assets/js/app.js
@@ -0,0 +1,39 @@
+
+$(function(){
+
+
+    // slider-> slick Slider, Owl Carousel, Swiper JS,
+
+    
+    // swiper slider start
+        // default:
+        // var swiper = new Swiper(".mySwiper", {});
+        // navigation:
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+            
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                // type: "progressbar",
+              },
+          });
+       
+
+    // swiper slider end
+
+    
+
+});
+
+
